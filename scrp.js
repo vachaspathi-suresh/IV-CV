f5913bef3757d97b496d68570d4d2f76d207227d
Juggernaut <ivsuresh03@gmail.com>
2022-05-14 01:37:38 +0530

    Commit without Themes and navigator

---
+++ scrp.js
@@ -0,0 +1,31 @@
+/******** Nav Bar Function ***********/
+$(".navico").on("click", function() {
+  $(".sidenav").toggleClass("navcont");
+});
+
+$(".ma").on("click", function() {
+  if ($(".sidenav").hasClass("navcont"))
+    $(".sidenav").removeClass("navcont");
+});
+
+
+/********* Theme Change Function **********/
+$(".sun-moon").on("click", function() {
+  $(".sun-moon").removeClass("daytonyt");
+  $(".sun-moon").addClass("daytonyt");
+  setTimeout(function() {
+    $("#title").toggleClass("night");
+    if ($("#title").hasClass("night")) {
+      $(".road").attr("src", "res/images/roadN.png");
+      $(".sun-moon").attr("src", "res/images/moon.png");
+    } else {
+      $(".road").attr("src", "res/images/roadM.png");
+      $(".sun-moon").attr("src", "res/images/sun.png");
+    }
+  }, 1700);
+
+  setTimeout(function() {
+    $(".sun-moon").removeClass("daytonyt");
+  }, 3200);
+
+});
